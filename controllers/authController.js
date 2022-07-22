167627a8f442df618468d62e38b46ba0aef23479
David <david@Davids-MacBook-Pro.local>
2022-07-22 17:40:04 +0900

    Add simple error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,17 @@
-exports.signUp = function (req, res) {
+const User = require("../models/userModel");
+
+const catchAsync = require("../utils/catchAsync");
+
+exports.signUp = catchAsync(async function(req, res) {
+    const { name, email, password } = req.body;
+
+    await User.create({
+        name,
+        email,
+        password
+    })
+
     res.status(201).json({
         message: "Successful!"
     })
-}
+});
