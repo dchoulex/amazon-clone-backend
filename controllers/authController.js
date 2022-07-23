70f031cbbcf4f206b01a80d44df2c9048a0c4580
David <david@Davids-MacBook-Pro.local>
2022-07-23 11:52:11 +0900

    Implement JWT authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,65 @@
 const User = require("../models/userModel");
+const AppError = require("../utils/AppError");
+const jwt = require("jsonwebtoken");
 
 const catchAsync = require("../utils/catchAsync");
 
 exports.signUp = catchAsync(async function(req, res) {
     const { name, email, password } = req.body;
 
-    await User.create({
+    const newUser = await User.create({
         name,
         email,
         password
-    })
+    });
 
-    res.status(201).json({
-        message: "Successful!"
-    })
+    createOrSendToken(newUser, 201, res);
 });
+
+exports.login = catchAsync(async function(req, res, next) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new AppError(400, "Please provide email and password!"));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+    const isValid = await user.validatePassword(password, user.password);
+
+    if (!user || !isValid) {
+        return next(new AppError(401, "Incorrect email or password"));
+    }
+
+    createOrSendToken(user, 200, res);
+});
+
+function createOrSendToken(user, statusCode, res) {
+    const token = signToken(user._id);
+
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+        secure: true
+    };
+
+    res.cookie("jwt", token, cookieOptions);
+
+    const userData = {
+        name: user.name,
+        email: user.email
+    }
+
+    res.status(statusCode).json({
+        status: "success",
+        token,
+        data: userData
+    })
+}
+
+function signToken(id) {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+}
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const validator = require("validator")
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -28,6 +29,22 @@ const userSchema = new mongoose.Schema({
     },
     phoneNumber: String
 });
+
+userSchema.pre("save", async function(next) {
+    this.password = await bcrypt.hash(this.password, 12);
+
+    next();
+});
+
+userSchema.pre(/^find/, function(next) {
+    this.find({ active: {$ne : false} });
+
+    next();
+});
+
+userSchema.methods.validatePassword = async function(candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model("User", userSchema);
 
