487de7d0a76d38be61e67b110ce610164584efe0
David <david@Davids-MacBook-Pro.local>
2022-07-25 23:07:22 +0900

    Add shipping cost data

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const authRouter = require("./routes/authRoutes");
 const productRouter = require("./routes/productRoutes");
 const cartRouter = require("./routes/cartRoutes");
+const orderRouter = require("./routes/orderRoutes");
 const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/carts", cartRouter);
+app.use("api/v1/orders", orderRouter);
 
 app.use(globalErrorHandler);
 
